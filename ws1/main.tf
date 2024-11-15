2f9f94156fd7cf7b78735dcb2e2916f455ae1b51
tylerwolf <twolf@hashicorp.com>
2024-11-15 11:49:50 -0700

    Make bucket public

--- ws1/main.tf
+++ ws1/main.tf
@@ -17,4 +17,11 @@ provider "aws" {
 
 resource "aws_s3_bucket" "bucket" {
   bucket = "rocket2024-ws1-${var.team-name}"
+  acl = "public-read"
+}
+
+resource "aws_s3_object" "object" {
+  bucket = aws_s3_bucket.bucket.id
+  key = "file"
+  content_base64 = "TGVnZW5kcyBvZiB0aGUgSGlkZGVuIFRlbXBsZSB3YXMgcmVjb3JkZWQgaW4gZnJvbnQgb2YgYSBsaXZlIHN0dWRpbyBhdWRpZW5jZSBpbiBOaWNrZWxvZGVvbiBTdHVkaW9zIGF0IFVuaXZlcnNhbCBTdHVkaW9zLCBGbG9yaWRhLg=="
 }
